d0879b3cc82b14e649ff7e2efcf378db26c9960b
Lucas Gabriel <gomesborgesbiel@gmail.com>
2025-01-17 00:55:02 -0300

    1701

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+    const html = document.documentElement
+    // if (html.classList.contains("Light")) {
+    //   html.classList.remove("light")
+    // } else {
+    //   html.classList.add("light")
+    // }
+    html.classList.toggle("light")
+  }
+  
